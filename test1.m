c0a58437134a7d637d5b8e1849d8d3d9bcd70861
Zaeem Riaz <razzaeem0@gmail.com>
2019-11-30 18:31:10 +0500

    testing 3

--- test1.m
+++ test1.m
@@ -1,6 +1,6 @@
 
-I1 = imread('img\car6.jpeg');
-I2 = imread('img\car7.jpeg');
+I1 = imread('img\car4.jpeg');
+I2 = imread('img\car5.jpeg');
 
 
 % I3 = labelingAlgorithm(I1);
@@ -15,20 +15,18 @@ I2 = imread('img\car7.jpeg');
 
 
 
-% I1 = rgb2gray(I1);
-% I2 = rgb2gray(I2);
+I1 = rgb2gray(I1);
+I2 = rgb2gray(I2);
 
 % figure, imshow(I1,'InitialMagnification',100);
 
-I3 = subtractImages(I1(;;1), I2);
-
-figure, imshow(I3,'InitialMagnification',100);
+I3 = subtractImages(I1, I2);
 
 
-I4 = automaticThresholding(I3);
 
-
+I4 = automaticThresholding(I7);
 figure, imshow(I4,'InitialMagnification',100);
 
 % imwrite(I3, 'img\sub.jpeg');
 % imwrite(I4, 'img\otsu.jpeg');
+
