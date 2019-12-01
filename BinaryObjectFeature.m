af49b2eb745a15076c309d836b29d4295eaf4e0a
Zaeem Riaz <razzaeem0@gmail.com>
2019-12-01 15:55:05 +0500

    finding coordinates

--- BinaryObjectFeature.m
+++ BinaryObjectFeature.m
@@ -2,7 +2,7 @@ classdef (ConstructOnLoad = true) BinaryObjectFeature
     methods
 
         % area
-        function A1 = area(~, I1)
+        function A = area(~, I1)
             
             % get image size
             [r1, c1] = size(I1);
@@ -18,18 +18,11 @@ classdef (ConstructOnLoad = true) BinaryObjectFeature
                     end
                 end
             end
-            j = 1;
-            A1 = zeros(2, 1, 'uint32');
-            for i = 1 : 10
-                if A(i) ~= 0
-                    A1(j) = A(i);
-                    j = j + 1;
-                end
-            end
+            
         end
 
         % center of area
-        function coord = centerOfArea(obj, I1)
+        function coord1 = centerOfArea(obj, I1)
             % get image size
             [r1, c1] = size(I1);
             
@@ -47,113 +40,14 @@ classdef (ConstructOnLoad = true) BinaryObjectFeature
                 end
             end
             
-            % get the 2 objects first
-            j = 1;
-            coord = zeros(2, 2, 'uint32');
-            for i = 1 : 10
-                if coord1(i, 1) ~= 0 && coord1(i, 2) ~= 0 
-                    coord(j, 1) = coord1(i, 1);
-                    coord(j, 2) = coord1(i, 2);
-                    j = j + 1;
-                end
-            end
             
             A = obj.area(I1);
             % divide by area
-            for k = 1 : 2
-                coord(k, 1) = coord(k, 1) / A(k);
-                coord(k, 2) = coord(k, 2) / A(k);
+            for k = 1 : 10
+                coord1(k, 1) = coord1(k, 1) / A(k);
+                coord1(k, 2) = coord1(k, 2) / A(k);
             end
         end
 
-        % horizontal projection
-        function P = horizontalProjection(~, I1)
-            % get image size
-            [r1, c1] = size(I1);
-            
-            % get labelled image
-            ILabel = labelingAlgorithm(I1);
-            P1 = zeros(10, r1, 'uint32');
-            
-            % get projection of image
-            for r = 1 : r1
-                for c = 1 : c1
-                    if ILabel(r, c) ~= 0
-                        P1(ILabel(r, c), r) = P1(ILabel(r, c), r) + 1;
-                    end
-                end
-            end
-            
-            % get the 2 objects first with projection of objects at first
-            j = 1;
-            P = zeros(2, r1, 'uint32');
-            for i = 1 : 10
-                empty = true;
-                s = 0;
-                l = 0;
-                for r = 1 : r1
-                    if P1(i, r) ~= 0
-                        empty = false;
-                        % find start of projection
-                        if s == 0
-                            s = r;
-                        end
-                        % find length of projection
-                        l = l + 1;
-                    end
-                end
-                if empty == false
-                    for r = 1 : l
-                        P(j, r) = P1(i, s+r-1);
-                    end
-                    j = j + 1;
-                end
-            end
-        end
-
-        % vertical Projection
-        function P = vaerticalProjection(~, I1)
-            % get image size
-            [r1, c1] = size(I1);
-            
-            % get labelled image
-            ILabel = labelingAlgorithm(I1);
-            P1 = zeros(10, r1, 'uint32');
-            
-            % get projection of image
-            for r = 1 : r1
-                for c = 1 : c1
-                    if ILabel(r, c) ~= 0
-                        P1(ILabel(r, c), c) = P1(ILabel(r, c), c) + 1;
-                    end
-                end
-            end
-            
-            % get the 2 objects first with projection of objects at first
-            j = 1;
-            P = zeros(2, c1, 'uint32');
-            for i = 1 : 10
-                empty = true;
-                s = 0;
-                l = 0;
-                for c = 1 : c1
-                    if P1(i, c) ~= 0
-                        empty = false;
-                        % find start of projection
-                        if s == 0
-                            s = c;
-                        end
-                        % find length of projection
-                        l = l + 1;
-                    end
-                end
-                if empty == false
-                    for c = 1 : l
-                        P(j, c) = P1(i, s+c-1);
-                    end
-                    j = j + 1;
-                end
-            end
-        end
     end
 end
--- test1.m
+++ test1.m
@@ -8,30 +8,40 @@ I1 = rgb2gray(f1);
 I2 = rgb2gray(f2);
 
 I3 = subtractImages(I1, I2);
-% figure, imshow(I3,'InitialMagnification',100);
-
 
 I4 = automaticThresholding(I3);
-% figure, imshow(I4,'InitialMagnification',100);
 
 morph = Morphology;
-I5 = morph.dilation(I5);
-I5 = morph.dilation(I5);
-
-figure, imshow(I5,'InitialMagnification',100);
-
-% I3 = labelingAlgorithm(I1);
+I5 = morph.dilation(I4);
+I6 = morph.dilation(I5);
 
 bof = BinaryObjectFeature;
-area = bof.area(I1);
-coord = bof.centerOfArea(I1);
-% area = bof.horizontalProjection(I1);
-% area = bof.vaerticalProjection(I1);
+area = bof.area(I6);
+coord = bof.centerOfArea(I6);
 
-% I3 = cannyEdgeDetection(I1);
+% get index for 2 largest objects
+maxI = 0;
+maxV = 0;
+smaxI = 0;
+smaxV = 0;
+for x = 1 : size(area)
+    if area(x) > maxV
+        smaxV = maxV;
+        smaxI = maxI;
+        maxV = area(x);
+        maxI = x;
+    else
+        if area(x) > smaxV
+            smaxV = area(x);
+            smaxI = x;
+        end
+    end
+end
 
-% I4 = automaticThresholding(I7);
-% figure, imshow(I4,'InitialMagnification',100);
+x1 = coord(maxI, 1);
+y1 = coord(maxI, 2);
+x2 = coord(smaxI, 1);
+y2 = coord(smaxI, 2);
 
 % imwrite(I5, 'img\2xdilation.jpeg');
 % imwrite(I4, 'img\otsu.jpeg');
